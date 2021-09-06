328984ba26ea68a562c992d68d1eb4f078945d58
janhvi <janhvi1009@gmail.com>
2021-09-06 11:18:51 +0530

    okay

--- stores/views.py
+++ stores/views.py
@@ -1,8 +1,1 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-from .models import FoodStore
-from .serializers import *
-
-
+# ejdkfv
